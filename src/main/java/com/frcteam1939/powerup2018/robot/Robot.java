e3de4618132b8e9f22b266b38ffdce0c163cf825
azhawa <azhawa@hotmail.com>
2018-01-06 13:44:36 -0600

    Setup of Project

--- src/main/java/com/frcteam1939/powerup2018/robot/Robot.java
+++ src/main/java/com/frcteam1939/powerup2018/robot/Robot.java
@@ -3,7 +3,7 @@ package com.frcteam1939.powerup2018.robot;
 
 import com.frcteam1939.powerup2018.robot.subsystems.Drivetrain;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the IterativeRobot documentation. If you change the name of this class or the package after creating this project, you must also update the manifest file in the resource directory.
  */
-public class Robot extends IterativeRobot {
+public class Robot extends TimedRobot {
 
 	public static Drivetrain drivetrain = new Drivetrain();
 	public static OI oi;
