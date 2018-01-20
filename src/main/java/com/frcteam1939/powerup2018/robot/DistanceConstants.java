754f66ee1d48d4973d674eda7d8ef22f2c450b7a
MiniCIM <azhawa@hotmail.com>
2018-01-20 12:46:09 -0600

    Updating Distance Constants

--- src/main/java/com/frcteam1939/powerup2018/robot/DistanceConstants.java
+++ src/main/java/com/frcteam1939/powerup2018/robot/DistanceConstants.java
@@ -5,7 +5,15 @@ package com.frcteam1939.powerup2018.robot;
  */
 public class DistanceConstants {
 
+	// Drivetrain Constants
 	public static final double SWITCH = 168;
 	public static final double SCALE = 324;
 	public static final double AUTO_LINE = 120;
+
+	// Elevator Constants
+	public static final double HIGH_LIMIT = 0;
+	public static final double LOW_LIMIT = 0;
+	public static final double PORTAL = 27;
+	public static final double ELEVATOR_SWITCH = 30;
+	public static final double ELEVATOR_SCALE = 75;
 }
