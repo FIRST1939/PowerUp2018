427e78e6314e91ece2327defe645d4c0a1a710ea
MiniCIM <azhawa@hotmail.com>
2018-01-25 20:47:32 -0600

    Adding Test Function

--- src/main/java/com/frcteam1939/powerup2018/robot/subsystems/CubeManipulator.java
+++ src/main/java/com/frcteam1939/powerup2018/robot/subsystems/CubeManipulator.java
@@ -10,17 +10,13 @@ public class CubeManipulator extends Subsystem {
 
 	public static final double OUT_SPEED = 1.0;
 	public static final double IN_SPEED = -1.0;
-	private TalonSRX masterTalon = new TalonSRX(RobotMap.cubeManipulatorTalon);
-	private TalonSRX slaveTalon = new TalonSRX(RobotMap.cubeManipulatorTalon);
+	private TalonSRX talon = new TalonSRX(RobotMap.cubeManipulatorTalon);
 
 	public void CubeIntake() {
 
 	}
 
-	public CubeManipulator() {
-		//	slaveTalon.setInverted(true);
-		this.slaveTalon.follow(this.masterTalon);
-	}
+	public CubeManipulator() {}
 
 	@Override
 	public void initDefaultCommand() {
@@ -28,6 +24,6 @@ public class CubeManipulator extends Subsystem {
 	}
 
 	public void set(double value) {
-		this.masterTalon.set(ControlMode.PercentOutput, value);
+		this.talon.set(ControlMode.PercentOutput, value);
 	}
 }
