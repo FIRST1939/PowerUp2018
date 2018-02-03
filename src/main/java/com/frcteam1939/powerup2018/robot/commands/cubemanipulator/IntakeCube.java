0d34985ad9b79bffdce3967f0f897bc4e2b42d9f
learnSola <sdugbo@learnscienceandmathclub.org>
2018-02-03 15:39:35 -0600

    Stop cubemanipulator
    Added stop cube manipulator for intake and output cube

--- src/main/java/com/frcteam1939/powerup2018/robot/commands/cubemanipulator/IntakeCube.java
+++ src/main/java/com/frcteam1939/powerup2018/robot/commands/cubemanipulator/IntakeCube.java
@@ -1,5 +1,6 @@
 package com.frcteam1939.powerup2018.robot.commands.cubemanipulator;
 
+import com.frcteam1939.powerup2018.robot.Robot;
 import com.frcteam1939.powerup2018.robot.subsystems.CubeManipulator;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -9,6 +10,7 @@ public class IntakeCube extends CommandGroup {
 	public IntakeCube() {
 		this.addSequential(new CubeManipulatorLower());
 		this.addSequential(new SetCubeManipulatorSpeed(CubeManipulator.IN_SPEED));
+		if(!Robot.cubeManipulator.haveCube()){Robot.cubeManipulator.set(0);}
 		this.addSequential(new CubeManipulatorWheelsIn());
 		this.addSequential(new CubeManipulatorRaise());
 	}
--- src/main/java/com/frcteam1939/powerup2018/robot/commands/cubemanipulator/OutputCube.java
+++ src/main/java/com/frcteam1939/powerup2018/robot/commands/cubemanipulator/OutputCube.java
@@ -1,5 +1,6 @@
 package com.frcteam1939.powerup2018.robot.commands.cubemanipulator;
 
+import com.frcteam1939.powerup2018.robot.Robot;
 import com.frcteam1939.powerup2018.robot.subsystems.CubeManipulator;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -9,6 +10,7 @@ public class OutputCube extends CommandGroup {
 	public OutputCube() {
 		this.addSequential(new CubeManipulatorMiddle());
 		this.addSequential(new SetCubeManipulatorSpeed(CubeManipulator.OUT_SPEED));
+		if(Robot.cubeManipulator.haveCube()){Robot.cubeManipulator.set(0);}
 		this.addSequential(new CubeManipulatorRaise());
 	}
 }
