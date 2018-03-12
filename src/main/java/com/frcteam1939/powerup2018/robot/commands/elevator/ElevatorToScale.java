55589c16107f834ced275b780a984ad0d02becbf
MiniCIM <azhawa@hotmail.com>
2018-03-11 20:37:06 -0500

    Set Elevator Heights

---
+++ src/main/java/com/frcteam1939/powerup2018/robot/commands/elevator/ElevatorToScale.java
@@ -0,0 +1,14 @@
+package com.frcteam1939.powerup2018.robot.commands.elevator;
+
+import com.frcteam1939.powerup2018.util.Wait;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class ElevatorToScale extends CommandGroup {
+
+	public ElevatorToScale() {
+		this.addSequential(new SetElevatorMotorSpeed(1));
+		this.addSequential(new Wait(2));
+		this.addSequential(new SetElevatorMotorSpeed(0));
+	}
+}
